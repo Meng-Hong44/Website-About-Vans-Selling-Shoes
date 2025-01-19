6ba60392488102dad313d3aa285700018fe4a31a
Menghong Lay <menghonglay732@gmail.com>
2025-01-20 02:20:45 +0700

    add 1

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const menutoggle = document.getElementById('menu-toggle');
-const nav = document.getElementById('nav-links');
-// menutoggle.addEventListener('click', () => {
-//     nav.style.display = 'block';
-// });
-const narv = 0;
-if(nav==narv){
-    menutoggle.addEventListener('click', () => {
-        nav.style.display = 'none';
-    });
-    nav++;
-}
-else{
-    menutoggle.addEventListener('click', () => {
-        nav.style.display = 'block';
 
-    });
-}
 AOS.init({
     duration:1200,
 })
+//heart icon event
+function iconHeart(event){
+    const heart=event.target;
+        if(heart.classList.contains('fa-regular')){
+            heart.classList.add('fa-solid');
+            heart.classList.remove('fa-regular');
+        }
+        else{
+            heart.classList.add('fa-regular');
+            heart.classList.remove('fa-solid');
+        }
+}
+const hearts=document.querySelectorAll(".fa-heart");
+hearts.forEach((heart)=>{
+    heart.addEventListener("click",iconHeart);
+});
+
